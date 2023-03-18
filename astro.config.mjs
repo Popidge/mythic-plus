77d068708576c36f98c636085af41f516ef05bb4
Jamie Taylor <j.taylor.1990@gmail.com>
2023-03-18 19:45:12 +0000

    fixed tailwind dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from 'astro/config';
 import svelte from "@astrojs/svelte";
 import tailwind from '@astrojs/tailwind';
-
 import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
